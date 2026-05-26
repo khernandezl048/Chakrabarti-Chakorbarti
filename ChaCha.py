39c933bfea31795d150c95da0a9b8688d9954467
khernandezl048 <khernandezl@unal.edu.co>
2026-05-25 19:38:52 -0500

    Datos de full

--- ChaCha.py
+++ ChaCha.py
@@ -3,24 +3,15 @@ import networkx as nx
 import time
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
-from numba import njit
 from scipy.stats import gaussian_kde
-from scipy.linalg import eigvals
-from scipy.linalg import expm
-from matplotlib import rc
+from joblib import Parallel, delayed
 
 
 #-----------------------------------FUNCIONES--------------------------------------
 
-def Simulacion(M, A, Money, T_max, lamb):
+def Simulacion(M, edges, prob, Money, T_max, lamb):
     m = Money.copy()
     
-    # Precalcular edges y probabilidades una sola vez
-    edges = np.argwhere(A > 0)
-    A_values = A[edges[:, 0], edges[:, 1]]
-    prob = A_values / A_values.sum()
-    
-    # Pregenerar todos los números aleatorios de una vez
     indices = np.random.choice(len(edges), size=T_max, p=prob)
     epsilons = np.random.uniform(0, 1, size=T_max)
     
@@ -28,7 +19,9 @@ def Simulacion(M, A, Money, T_max, lamb):
         i, j = edges[indices[t]]
         epsilon = epsilons[t]
         
-        delta_m = (1 - lamb) * (m[i] - epsilon * (m[i] + m[j]))
+        #delta_m = (1 - lamb) * (m[i] - epsilon * (m[i] + m[j]))
+        delta_m = (1 - lamb) * (epsilon*m[j] - (1-epsilon) * (m[i]))
+        
         
         if m[i] + delta_m < 0 or m[j] - delta_m < 0:
             continue
@@ -41,25 +34,35 @@ def Simulacion(M, A, Money, T_max, lamb):
 
 #-----------------------------------SIMULACION--------------------------------------
 
-T_max = int(1e6)
-N_simulations = 100
+T_max = int(1e5)
+N_simulations = int(1e4)
 
-M = 500
+M = 5e4
 N = 500
 
-lamb = 0.0
-G =  nx.complete_graph(N) 
+lamb = 0.1
+G = nx.complete_graph(N)
 A = nx.to_numpy_array(G)
 Money = np.full(N, M/N)
-Money_prom = np.zeros(len(Money))
 
+# Precalcular edges y probabilidades una sola vez
+'''
+edges = np.argwhere(A > 0)
+prob = A[edges[:, 0], edges[:, 1]]
+prob /= prob.sum()
+'''
+edges = np.array(list(G.edges()), dtype=np.int32)
+prob = np.ones(len(edges), dtype=np.float64)
+prob /= prob.sum()
 start_time_total = time.time()
-for sim in range(N_simulations):
-    Money_prom += Simulacion(M, A, Money, T_max, lamb)
-    print(f"\n  Simulación {sim+1}/{N_simulations} lista.")
-    print(f"--- Tiempo parcial: {(time.time() - start_time_total)/60:.2f} minutos ---")
+
+results = Parallel(n_jobs=8, verbose=10)(
+    delayed(Simulacion)(M, edges, prob, Money, T_max, lamb)
+    for sim in range(N_simulations)
+)
+
+Money_prom = results
 
 print(f"\n--- Tiempo total: {(time.time() - start_time_total)/60:.2f} minutos ---")
 
-Money_prom /= N_simulations
-np.save('Pm_full.npy',Money_prom)
+np.save('Pm_full.npy', Money_prom)
