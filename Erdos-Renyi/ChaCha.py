4ff71af4f241c9be7c8964a256de95146e59fdf5
khernandezl048 <khernandezl@unal.edu.co>
2026-05-25 20:46:46 -0500

    a

--- Erdos-Renyi/ChaCha.py
+++ Erdos-Renyi/ChaCha.py
@@ -1,11 +1,7 @@
 import numpy as np
 import networkx as nx
 import time
-import matplotlib.pyplot as plt
-import matplotlib.cm as cm
-from scipy.stats import gaussian_kde
 from joblib import Parallel, delayed
-
 
 #-----------------------------------FUNCIONES--------------------------------------
 
@@ -19,9 +15,7 @@ def Simulacion(M, edges, prob, Money, T_max, lamb):
         i, j = edges[indices[t]]
         epsilon = epsilons[t]
         
-        #delta_m = (1 - lamb) * (m[i] - epsilon * (m[i] + m[j]))
-        delta_m = (1 - lamb) * (epsilon*m[j] - (1-epsilon) * (m[i]))
-        
+        delta_m = (1 - lamb) * (epsilon*m[j] - (1-epsilon) * m[i])
         
         if m[i] + delta_m < 0 or m[j] - delta_m < 0:
             continue
@@ -31,7 +25,6 @@ def Simulacion(M, edges, prob, Money, T_max, lamb):
     
     return m
 
-
 #-----------------------------------SIMULACION--------------------------------------
 
 T_max = int(1e5)
@@ -40,29 +33,31 @@ N_simulations = int(1e4)
 M = 5e4
 N = 500
 
-lamb = 0.1
-G = nx.complete_graph(N)
-A = nx.to_numpy_array(G)
+G = nx.erdos_renyi_graph(N, p=0.3) #
 Money = np.full(N, M/N)
 
-# Precalcular edges y probabilidades una sola vez
-'''
-edges = np.argwhere(A > 0)
-prob = A[edges[:, 0], edges[:, 1]]
-prob /= prob.sum()
-'''
 edges = np.array(list(G.edges()), dtype=np.int32)
 prob = np.ones(len(edges), dtype=np.float64)
 prob /= prob.sum()
+
+lambdas = np.round(np.arange(0.0, 1.0, 0.1), 1)  # [0.0, 0.1, ..., 0.9]
+
 start_time_total = time.time()
 
-results = Parallel(n_jobs=8, verbose=10)(
-    delayed(Simulacion)(M, edges, prob, Money, T_max, lamb)
-    for sim in range(N_simulations)
-)
-
-Money_prom = results
+for lamb in lambdas:
+    print(f"\n{'='*40}")
+    print(f"Simulando lamb = {lamb}")
+    print(f"{'='*40}")
+    
+    start = time.time()
+    
+    results = Parallel(n_jobs=8, verbose=10)(
+        delayed(Simulacion)(M, edges, prob, Money, T_max, lamb)
+        for sim in range(N_simulations)
+    )
+    
+    np.save(f'Pm_lamb{lamb:.1f}.npy', results)
+    
+    print(f"lamb={lamb:.1f} completado en {(time.time() - start)/60:.2f} min")
 
 print(f"\n--- Tiempo total: {(time.time() - start_time_total)/60:.2f} minutos ---")
-
-np.save('Pm_full.npy', Money_prom)
